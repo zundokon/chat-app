3d65a7053db8e73904c0e5b65ccc3c8b0d1a392d
zundokon <tete5337@yahoo.co.jp>
2023-07-27 12:55:06 +0900

    Active Storageの導入

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module ChatApp
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
