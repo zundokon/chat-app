939c0af0cf0862dca33ca4505c648cefeb91b2a4
zundokon <tete5337@yahoo.co.jp>
2023-07-27 17:59:07 +0900

    各モデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :room_users
   has_many :rooms, through: :room_users
-  has_many :messages
+  has_many :messages  
 
   validates :name, presence: true
 end
