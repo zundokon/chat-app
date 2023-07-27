cce9f1dcb83c6636c6f0d97aaf136b03f36fa21f
zundokon <tete5337@yahoo.co.jp>
2023-07-27 12:40:55 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,6 +15,12 @@ class RoomsController < ApplicationController
   def index
   end
 
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
   private
 
   def room_params
